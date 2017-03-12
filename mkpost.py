edb5a92a154c5c7b20dc0753d11d42a3a1025c58
Mahdi Abdinejadi <mehdi61b@gmail.com>
2017-03-12 14:48:34 +0100

    make post script template

---
+++ mkpost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+
+from __future__ import print_function
+import sys
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+
+""" Simple post creator script """
+
+def run():
+    ap = ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    ap.add_argument('postname', help='Set post name to create post file')
+    
+    args = ap.parse_args()
+
+
+
+
+
+
+if __name__ == "__main__":
+    run()
+    print ('**DONE**')
